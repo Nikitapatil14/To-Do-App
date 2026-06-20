488906a086474e01099dc5c77c24e5c27b59c0d7
Nikita Patil <nikitapatil930959@gmail.com>
2026-06-20 17:27:41 +0530

    Add function to create and delete tasks

---
+++ style.js
@@ -0,0 +1,19 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value;
+
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerHTML = `
+    ${taskText}
+    <button class="delete" onclick="this.parentElement.remove()">X</button>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
